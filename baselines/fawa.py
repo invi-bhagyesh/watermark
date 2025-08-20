edd646c558c1ad197bacb122f8db82bd7a734c11
invi_bhagyesh <104337027+invi-bhagyesh@users.noreply.github.com>
2025-08-21 00:10:04 +0530

    path fixed

--- baselines/fawa.py
+++ baselines/fawa.py
@@ -1,6 +1,7 @@
 import os
 import sys
 # sys.path.append('xx/ProTegO/')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import string
 import shutil
 import argparse
